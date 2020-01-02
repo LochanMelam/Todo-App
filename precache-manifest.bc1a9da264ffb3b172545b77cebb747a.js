b0f37ec6298e414fea52f1678df62792c7faafb2
LochanMelam <lochan99melam@gmail.com>
2020-01-02 12:39:21 +0530

    Updates

---
+++ precache-manifest.bc1a9da264ffb3b172545b77cebb747a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810cda8f2db69dfa7c69718c485ec01e",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "087eb417612f691c2eb1",
+    "url": "/Todo-App/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "43c1eeb206fc797c7230",
+    "url": "/Todo-App/static/js/2.05b4e2f0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Todo-App/static/js/2.05b4e2f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "087eb417612f691c2eb1",
+    "url": "/Todo-App/static/js/main.f8b059a8.chunk.js"
+  },
+  {
+    "revision": "1b9fba91f6e1c97db851",
+    "url": "/Todo-App/static/js/runtime-main.d26e8aea.js"
+  }
+]);
